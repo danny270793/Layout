c6735a89f18ddf2a3a41a3e8ef17c4095c52d836
Danny Vaca <danny270793@icloud.com>
2025-10-25 18:56:50 -0500

    refactor: update examples App component

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,6 +1,6 @@
-import type { ReactNode } from "react"
-import { Layout } from "@danny270793/layout"
-import "@danny270793/layout/dist/index.css"
+import type { ReactNode } from 'react';
+import { Layout } from '@danny270793/layout';
+import '@danny270793/layout/dist/index.css';
 
 export const App = (): ReactNode => {
   return (
@@ -9,7 +9,7 @@ export const App = (): ReactNode => {
         <h1>App bar</h1>
       </Layout.Header>
       <Layout.Content className="bg-blue-500">
-      <input type="text" placeholder="Search" />
+        <input type="text" placeholder="Search" />
         {Array.from({ length: 100 }).map((_, index) => (
           <h1 key={index}>item {index + 1}</h1>
         ))}
@@ -18,5 +18,5 @@ export const App = (): ReactNode => {
         <h1>Footer</h1>
       </Layout.Footer>
     </Layout>
-  )
-}
+  );
+};
