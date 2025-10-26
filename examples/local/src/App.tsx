54f865a7484a57603f02115e7428f71dc1bcb057
Danny Vaca <danny270793@icloud.com>
2025-10-26 00:09:04 -0500

    feat: add local examples

---
+++ examples/local/src/App.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from 'react';
+import { Layout } from '@danny270793/layout';
+
+export const App = (): ReactNode => {
+  return (
+    <Layout>
+      <Layout.Header className="bg-red-500">
+        <h1>App bar</h1>
+      </Layout.Header>
+      <Layout.Content className="bg-blue-500">
+        {Array.from({ length: 100 }).map((_, index) => (
+          <h1 key={index}>item {index + 1}</h1>
+        ))}
+      </Layout.Content>
+      <Layout.Footer className="bg-green-500">
+        <h1>Footer</h1>
+      </Layout.Footer>
+    </Layout>
+  );
+};
