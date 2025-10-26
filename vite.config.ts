4d30a6eb609de5ac03c2f3eb6b8c4f8c35745fea
Danny Vaca <danny270793@icloud.com>
2025-10-26 00:07:56 -0500

    chore: update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import path from 'node:path';
 import dts from 'vite-plugin-dts';
+import tailwindcss from '@tailwindcss/vite'
 import { peerDependencies } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     dts({ tsconfigPath: path.resolve(__dirname, 'tsconfig.app.json') }),
   ],
   build: {
